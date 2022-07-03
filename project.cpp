5f954eaa7a2f16a86e88b455b4ccae48d8e173fd
Alexander Zou <alexanderzou2@gmail.com>
2022-07-03 14:52:24 -0400

    skeleton of project file

---
+++ project.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void fetch(char** args, int& n, int& seed, int& lambda, int& bound, int& cs, int& alpha, int& slice) {
+	//use arguments as buffers
+}
+
+int main(int argc, char** argv) {
+	//error handling
+	
+	//fetch args
+	int n = 0, seed = 0, lambda = 0, bound = 0, cs = 0, alpha = 0, slice = 0;
+	fetch(argv, n, seed, lambda, bound, cs, alpha, slice);
+	
+	//do FCFS
+	
+	//do SJF
+	
+	//do SRT
+	
+	//do RR
+	
+	//output
+	printf("hello\n");
+}
